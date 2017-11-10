34780b902d586d1d213983b308e2b4f5528cbc9d
Bastien0406 <bastienmagnan1995@hotmail.com>
2017-11-10 10:18:30 +0100

    Menu

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ int main()
 		Lancement d'une partie avec 4 joueurs humains
 	*/
 	char tab[10][20];
-	int lig=2;
 
 	t_Partie game;
 	srand(time(NULL));
@@ -20,7 +19,7 @@ int main()
 	//initialisationPartie(&game, 14, 14, PARTIE_STANDARD, 8, JOUEUR_HUMAIN, RED, 6, 6, JOUEUR_HUMAIN, BLUE, 7, 7);
 
 	//printf("Initialisation faite\n");
-	choix_menu(tab, lig);
+	choix_menu(tab);
 
 
     return 0;
