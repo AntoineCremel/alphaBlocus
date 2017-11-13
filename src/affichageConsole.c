a6e605159aa9b686fc677581d8da1a1a018b77ca
Julien Bernard <Julien.bernard.1997@hotmail.fr>
2017-11-13 13:43:31 +0100

    Affichage en cours

--- src/affichageConsole.c
+++ src/affichageConsole.c
@@ -262,3 +262,56 @@ void affichageCompletJoueurConsole (t_Partie * game, t_Joueur * self)
         scrollToPrecedent(self);
     }
 }
+
+void xaffichageJoueurConsole(t_Joueur * self, int lig, int col)
+{
+	/*
+		Pour cette fonction j'ai choisi un affichage qui montre
+		la pièce active ainsi que deux pièces "au-dessus" et deux
+		pièces "en-dessous"
+	*/
+	// 0 Variables
+	// 0.0 On initialise ligne actuelle et col actuelle aux endroits où l'on veut
+	// 		commencer la fonction
+	int ligActuelle = lig + (I_TAB_PIECE-1)/2;
+	int colActuelle = col + J_TAB_PIECE;
+
+	// 1 On commence par colorer une zone taille fixe
+	for(int i=0; i < (I_TAB_PIECE + LINES_BETWEEN_PIECES_IN_SCROLL) * (PIECES_A_AFFICHER_AU_DESSUS + 1 + PIECES_A_AFFICHER_AU_DESSOUS); i++)
+	{
+		for(int j=-1; j < W_SCROLL; j++)
+		{
+			gotoligcol(lig + i, W_SQUARE_SCROLL * (col + j));
+			changeColour(0, SCROLL_BACK_COLOUR);
+			for(int k=0; k < W_SQUARE_SCROLL; k++)
+				printf(" ");
+		}
+	}
+
+	// 2 Affichage des deux pièces au-dessus
+	// 2.0 On scroll à travers la liste de chainee de joueur pour se mettre deux pieces plus haut
+	for(int i=0; i < PIECES_A_AFFICHER_AU_DESSUS; i++)
+		scrollToPrecedent(self);
+
+	// 2.1 Ensuite on affiche le nombre de pièces voulues
+	for(int i=0; i < PIECES_A_AFFICHER_AU_DESSUS + 1 + PIECES_A_AFFICHER_AU_DESSOUS; i++)
+	{
+		// 2.1.0 On affiche la pièce en cours
+		affichagePieceScrollConsole(self->ancre, ligActuelle, colActuelle, self->couleur);
+		//	2.1.1 On se met en place pour afficher la pièce suivante
+		ligActuelle += LINES_BETWEEN_PIECES_IN_SCROLL + I_TAB_PIECE;
+		colActuelle = col + J_TAB_PIECE;
+		//	2.1.2 On scroll à travers la liste de pièces
+		if(i != PIECES_A_AFFICHER_AU_DESSUS + PIECES_A_AFFICHER_AU_DESSOUS)
+			scrollToSuivant(self);
+	}
+
+	// 3 On scroll pour revenir à l'ancre de départ
+	for(int i=0; i < PIECES_A_AFFICHER_AU_DESSOUS; i++)
+		scrollToPrecedent(self);
+
+
+	// Remise à 0 du pointeur
+	gotoligcol(0, 0);
+	changeColour(BASE_TEXT, BASE_BACKGROUND);
+}
